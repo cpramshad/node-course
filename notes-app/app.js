d72db61929d4c11890d45bf9ffa3e9e7d65f80cb
cpramshad <cpramshad@gmail.com>
2021-01-22 13:58:00 +0400

    [feature] - 11 - adding npm package validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,11 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
+
 
 const notes = getNotes();
 console.log(notes);
+
+console.log(validator.isEmail('a@b.com'));
 
 
 
