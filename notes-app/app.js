f248ffd8cc84c8b3d531dd621fd879c4fc03f888
cpramshad <cpramshad@gmail.com>
2021-01-22 19:36:43 +0400

    [feature] - 16, 17 - Added yargs package to parse command line args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,70 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
 
-const notes = getNotes();
-console.log(notes);
 
-console.log(chalk.green('Success!'));
+// add, remove, read, list
+
+yargs.version('1.1.0')
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'list note',
+    handler: function () {
+        console.log('list note!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read note',
+    handler: function () {
+        console.log('Read note!')
+    }
+})
+
+yargs.parse();
+
+// console.log(process.argv);
+//console.log(yargs.argv);
 
 
+// const notes = getNotes();
+// console.log(notes);
+// console.log(chalk.green('Success!'));
 // const add = require('./utils.js');
 // const sum = add(5, -1);
 // console.log(sum);
